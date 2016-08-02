7e75a121eca96f4e47cfc4734b388ea26e0d19c3
Nick Pachulski <nmpach@mac.com>
2016-08-01 22:29:29 -0400

    Initial Commit

---
+++ inject-dependencies-to-test-drive-swift-appsTests/inject_dependencies_to_test_drive_swift_appsTests.swift
@@ -0,0 +1,36 @@
+//
+//  inject_dependencies_to_test_drive_swift_appsTests.swift
+//  inject-dependencies-to-test-drive-swift-appsTests
+//
+//  Created by Nicholas Pachulski on 8/1/16.
+//  Copyright © 2016 Nicholas Pachulski. All rights reserved.
+//
+
+import XCTest
+@testable import inject_dependencies_to_test_drive_swift_apps
+
+class inject_dependencies_to_test_drive_swift_appsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
