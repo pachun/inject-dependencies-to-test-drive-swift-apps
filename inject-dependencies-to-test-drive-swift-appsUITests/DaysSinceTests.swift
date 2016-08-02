52eace7e3bbc8b3ccf5c30a22bcb61bdbf2d5b41
Nick Pachulski <nick@pachulski.me>
2016-08-02 13:03:20 -0400

    Create valid but inefficient UI test.

--- inject-dependencies-to-test-drive-swift-appsUITests/DaysSinceTests.swift
+++ inject-dependencies-to-test-drive-swift-appsUITests/DaysSinceTests.swift
@@ -22,4 +22,57 @@ class DaysSinceTests: XCTestCase {
         XCTAssert(app.cells.elementBoundByIndex(0).staticTexts["1"].exists)
         XCTAssert(app.cells.elementBoundByIndex(1).staticTexts["0"].exists)
     }
+
+    func test_TappingTheDaysSincePlusButtonElevenOrMoreTimes_OnlyShowsTheTenMostRecentDaysSince() {
+        let app = XCUIApplication()
+        app.launch()
+
+        XCTAssertEqual(app.cells.count, 0)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 1)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 2)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 3)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 4)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 5)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 6)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 7)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 8)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 9)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 10)
+        XCTAssert(app.cells.elementBoundByIndex(9).staticTexts["0"].exists)
+
+        app.buttons["Add"].tap()
+
+        XCTAssertEqual(app.cells.count, 10)
+        XCTAssert(app.cells.elementBoundByIndex(9).staticTexts["1"].exists)
+    }
 }
